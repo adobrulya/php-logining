fe8da478db60b8723673fbaaa6a1a9a1368057aa
Andrey Dobrulya <adobrulya@gmail.com>
2021-05-07 08:20:47 +0300

    Исправил скрипт валидации пользователя

--- validation.php
+++ validation.php
@@ -9,53 +9,20 @@ or die("Error " . mysqli_error($link));
 $name = $_POST['user'];
 $pass = $_POST['password'];
 
-//Проверка имени пользователя с помощью регулярного выражения
-//чтобы имя было буквы, знак подчёркивания и цифры
-if(!preg_match($exp, $name))
-{
-    header('location:login.php');
-    exit();
-}
-//Проверка на первый символ букву
-/*if (preg_match('/\D/', $name))
-{
-    header('location:login.php');
-    exit();
-}
-*/
-//Проверка на длину имени
-if (strlen($name) <= 2)
-{
-    //echo strlen($name);
-    header('location:login.php');
-    exit();
-}
-
-$query = "SELECT name FROM usertable WHERE name='$name'";
-$result = mysqli_query($link, $query) or die("Error " . mysqli_error($link));
-$num = mysqli_num_rows($result);
-if($num >0)
-{
-    $query_in = "SELECT name FROM usertable WHERE name = '$name' && password = '$pass'";
-    $result = mysqli_query($link, $query_in) or die("Error " . mysqli_error($link));
-    $row_query_in = mysqli_num_rows($result);
-    if($row_query_in == 1)
-    {   //echo "home";
-        $_SESSION['username'] = $name;
-        header('location:home.php');
-    }
-    else
-    {
-        //echo "Login exist";
-        header('location:login.php');
-    }
+$query_in = "SELECT name FROM usertable WHERE name = '$name' && password = '$pass'";
+$result = mysqli_query($link, $query_in) or die("Error " . mysqli_error($link));
+$row_query_in = mysqli_num_rows($result);
+if($row_query_in == 1)
+{   //echo "home";
+    $_SESSION['username'] = $name;
+    header('location:home.php');
 }
 else
 {
-    $reg = "insert into usertable(name, password) values ('$name', '$pass')";
-    mysqli_query($link, $reg);
-    echo "Registration Successful";
+    //echo "Login exist";
+    header('location:login.php');
 }
+
 
 
 mysqli_close($link);
